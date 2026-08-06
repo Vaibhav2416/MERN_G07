d8fcdb1df2e7d4bfc6d5f21215decad1b5fb9081
Vaibhav Sultane <vaibhavsultane1997@gmail.com>
2026-08-06 17:07:35 +0530

    local storage 1

---
+++ phase_01_htmlcssjs/day_12/local_storage.js
@@ -0,0 +1,11 @@
+// Local Storage
+
+// It is a feature of web browser which allows you to 
+// store data permanently in your system
+
+// To store data inside local storage
+localStorage.setItem("student","Karik")
+
+// {"key":"Aman"} // {student:Kartik}
+// To retrive data from local storage
+localStorage.getItem("student")
